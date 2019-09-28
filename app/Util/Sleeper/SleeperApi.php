36b45d1c6ea9a04bf44d46c0388482c838f0f9d8
hotsaucejake <get@hotsaucejake.com>
2019-09-28 01:34:36 -0400

    sleeper api - leagues

--- app/Util/Sleeper/SleeperApi.php
+++ app/Util/Sleeper/SleeperApi.php
@@ -84,16 +84,88 @@ class SleeperApi
      * 
      */
 
+    // Full size URL
     public function getAvatarFull($avatarId = 'db7b742f6549b03367eb48ed1e328e5e')
     {
         return 'https://sleepercdn.com/avatars/'.$avatarId;
     }
 
-
+    // Thumbnail URL
     public function getAvatarThumbnail($avatarId = 'db7b742f6549b03367eb48ed1e328e5e')
     {
         return 'https://sleepercdn.com/avatars/thumbs/'.$avatarId;
     }
 
+
+    /*
+     ***************************************************************************
+     * Leagues
+     ***************************************************************************
+     *
+     * getUserLeagues($userId = 465649790558924800, $sport = 'nfl', $season = 2019)
+     * getLeague($leagueId = 465649923983929344)
+     * getLeagueRosters($leagueId = 465649923983929344)
+     * getLeagueUsers($leagueId = 465649923983929344)
+     * getLeagueMatchups($leagueId = 465649923983929344, $week = 1)
+     * getLeaguePlayoffWinnerBracket($leagueId = 465649923983929344)
+     * getLeaguePlayoffLoserBracket($leagueId = 465649923983929344)
+     * getLeagueTransactions($leagueId = 465649923983929344, $round = 1)
+     * getLeagueTradedPicks($leagueId = 465649923983929344)
+     * 
+     */
+
+    // Get all leagues for user
+    public function getUserLeagues($userId = 465649790558924800, $sport = 'nfl', $season = 2019)
+    {
+        return $this->publicRequest('GET', 'v1/user/'.$userId.'/leagues/'.$sport.'/'.$season);
+    }
+
+    // Get a specific league
+    public function getLeague($leagueId = 465649923983929344)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId);
+    }
+
+    // Getting rosters in a league
+    public function getLeagueRosters($leagueId = 465649923983929344)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId.'/rosters');
+    }
+
+    // Getting users in a league
+    public function getLeagueUsers($leagueId = 465649923983929344)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId.'/users');
+    }
+
+    // Getting matchups in a league
+    public function getLeagueMatchups($leagueId = 465649923983929344, $week = 1)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId.'/matchups/'.$week);
+    }
+
+    // Getting the playoff bracket
+    public function getLeaguePlayoffWinnerBracket($leagueId = 465649923983929344)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId.'/winners_bracket');
+    }
+
+    // Getting the playoff bracket
+    public function getLeaguePlayoffLoserBracket($leagueId = 465649923983929344)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId.'/loses_bracket');
+    }
+
+    // Get transactions
+    public function getLeagueTransactions($leagueId = 465649923983929344, $round = 1)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId.'/transactions/'.$round);
+    }
+
+    // Get traded picks
+    public function getLeagueTradedPicks($leagueId = 465649923983929344)
+    {
+        return $this->publicRequest('GET', 'v1/league/'.$leagueId.'/traded_picks');
+    }
     
 } 
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::view('/', 'welcome');
 
 Route::get('/test', function() {
-    dd(SleeperApi::getAvatarFull());
+    dd(SleeperApi::getLeagueTradedPicks());
 });
 
 Auth::routes();

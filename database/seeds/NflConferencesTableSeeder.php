18e159b4a6cd29f63c41058adb7a4bab1c84324e
hotsaucejake <get@hotsaucejake.com>
2019-11-02 11:39:59 -0400

    nfl conferences table seeder

---
+++ database/seeds/NflConferencesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\NFL\NflConference;
+use Illuminate\Database\Seeder;
+
+class NflConferencesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        NflConference::create([
+            'title' => 'American Football Conference',
+            'alias' => 'AFC',
+            'slug' => 'afc',
+        ]);
+        
+        NflConference::create([
+            'title' => 'National Football Conference',
+            'alias' => 'NFC',
+            'slug' => 'nfc',
+        ]);
+    }
+}

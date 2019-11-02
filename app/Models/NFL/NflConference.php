4aed6c9cdf95e0b369febb24d75909ce736d1cf4
hotsaucejake <get@hotsaucejake.com>
2019-11-02 11:35:47 -0400

    nfl conference model, migration, resource controller

---
+++ app/Models/NFL/NflConference.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\NFL;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NflConference extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'alias'];
+
+
+    public function nfl_divisions()
+    {
+        return $this->hasMany('App\Models\NFL\NflDivision');
+    }
+
+    
+    public function nfl_teams()
+    {
+        return $this->hasManyThrough('App\Models\NFL\NflTeam', 'App\Models\NFL\NflDivision');
+    }
+}
---
+++ database/migrations/2019_11_02_152540_create_nfl_conferences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNflConferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nfl_conferences', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('alias');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nfl_conferences');
+    }
+}

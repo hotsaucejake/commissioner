0e57aa51fec83077a6114a3d2fa5d772258cceb5
hotsaucejake <get@hotsaucejake.com>
2019-11-02 12:07:12 -0400

    nfl team model, migration, resource controller

---
+++ app/Models/NFL/NflTeam.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\NFL;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NflTeam extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nfl_division_id', 'title', 'name', 'slug'];
+
+
+    public function nfl_division()
+    {
+        return $this->belongsTo('App\Models\NFL\NflDivision');
+    }
+
+
+    public function nfl_conference()
+    {
+        return $this->nfl_division->nfl_conference();
+    }
+}

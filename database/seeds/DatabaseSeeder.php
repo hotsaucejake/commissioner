047a924c824f7a4b0628527462949d51f68d205a
hotsaucejake <get@hotsaucejake.com>
2019-11-02 12:10:16 -0400

    nfl teams table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(NflConferencesTableSeeder::class);
+        $this->call(NflDivisionsTableSeeder::class);
+        $this->call(NflTeamsTableSeeder::class);
     }
 }
